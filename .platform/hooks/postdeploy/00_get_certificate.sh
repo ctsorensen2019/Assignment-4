66a4415c526932313b4e2948de99a378418e32eb
Christian Sorensen <ctsorensen2019@gmail.com>
2024-11-23 13:07:58 -0700

    ya momma

---
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Place in .platform/hooks/postdeploy directory
+sudo certbot -n -d Bands-env.eba-jqtjgske.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email ctsorensen2019@gmail.com
